855e691fa238a2bec2d2ef512e785a2293ec5000
Brian <brianluiscoimbra@gmail.com>
2020-05-05 18:07:12 -0300

    adicionando array de contatos e exibindo a lista na tela principal

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,7 +7,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  public contatos;
+
   constructor(public navCtrl: NavController) {
+    this.contatos = [
+      {nome: "Fulano de Tal", email: "fulanodetal@email.com", telefone: "999999999"},
+      {nome: "Beltrano", email: "beltrano@email.com", telefone: "123454648"},
+      {nome: "José", email: "jose@email.com", telefone: "999999599"},
+      {nome: "Maria", email: "maria@email.com", telefone: "999999333"},
+      {nome: "Sicrano", email: "sicrano@email.com", telefone: "999499999"},
+    ];
 
   }
 
