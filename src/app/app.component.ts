ed43922acc21aee2b9ce6820d1225140a2ec7c76
Brian <brianluiscoimbra@gmail.com>
2020-05-05 23:09:06 -0300

    menu foi adicionado e esta funcionando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { InformaDadosPage } from '../pages/informa-dados/informa-dados';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
   rootPage:any = HomePage;
+
+  pages: Array<{title: string, component:any}>;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -16,7 +20,16 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      this.pages = [
+        {title: 'Home', component:HomePage},
+        {title: 'Informar dados', component:InformaDadosPage}
+      ];
     });
+  }
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
   }
 }
 
---
+++ src/pages/informa-dados/informa-dados.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { InformaDadosPage } from './informa-dados';
+
+@NgModule({
+  declarations: [
+    InformaDadosPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(InformaDadosPage),
+  ],
+})
+export class InformaDadosPageModule {}
