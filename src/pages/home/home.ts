cd4bdaab0faf729aab6e07f539c619118c3a29cc
Brian <brianluiscoimbra@gmail.com>
2020-05-05 21:51:42 -0300

    mensagem toast aparececendo ao clica no item da lista

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
+import { contato } from './modelos/contato';
 
 @Component({
   selector: 'page-home',
@@ -7,17 +8,30 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  public contatos;
+  public contatos: contato[];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private toastCtrl: ToastController) {
     this.contatos = [
-      {nome: "Fulano de Tal", email: "fulanodetal@email.com", telefone: "999999999"},
-      {nome: "Beltrano", email: "beltrano@email.com", telefone: "123454648"},
-      {nome: "José", email: "jose@email.com", telefone: "999999599"},
-      {nome: "Maria", email: "maria@email.com", telefone: "999999333"},
-      {nome: "Sicrano", email: "sicrano@email.com", telefone: "999499999"},
+      {nome: "Fulano de Tal", email: "fulanodetal@email.com", telefone: 999999999},
+      {nome: "Beltrano", email: "beltrano@email.com", telefone: 123454648},
+      {nome: "José", email: "jose@email.com", telefone: 999999599},
+      {nome: "Maria", email: "maria@email.com", telefone: 999999333},
+      {nome: "Sicrano", email: "sicrano@email.com", telefone: 999499999},
     ];
+  }
 
+  showToast(contatoSelecionado: contato) {
+    let toast = this.toastCtrl.create({
+      message: 'Nome do item selecionado: ' + contatoSelecionado,
+      duration: 3000,
+      position: 'bottom'
+    });
+  
+    toast.onDidDismiss(() => {
+      console.log('Toast fechado');
+    });
+  
+    toast.present();
   }
 
 }
---
+++ src/pages/home/modelos/contato.ts
@@ -0,0 +1,5 @@
+export interface contato{
+    nome: String;
+    email: String;
+    telefone: number;
+}
